2a515bff6d29e9e09ae18f74f49c083f7a6222da
LiJiasen-00921 <Lijiasen00921@gmail.com>
2019-10-15 10:26:16 +0800

    SJTU

--- adder/ctrl/build.sh
+++
@@ -1,1 +0,0 @@
-g++ *.cpp -std=c++14 -I /tmp/usr/local/include/ -L /tmp/usr/local/lib/ -lserial -lpthread -o ./ctrl
--- adder/ctrl/run.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tmp/usr/local/lib
-./ctrl $@
